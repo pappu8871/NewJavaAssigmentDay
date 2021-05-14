5dd6d193e52675d1650f7678ccd6041db0fbb80b
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-14 17:29:57 +0530

    Added:class EmpWageComputationUC1.java

---
+++ EmpWageComputationUC1.java
@@ -0,0 +1,18 @@
+package empwagecomputation;
+public class EmpWageComputationUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int IS_FULL_TIME = 1;
+		//Compitation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	
+		
+	}
+
+}
