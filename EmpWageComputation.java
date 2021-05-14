9e3e7b9b4fcc2f40d2821486ed41dcbf96fad421
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-14 17:26:15 +0530

    Added:Welcome EmpWageComputation.java

---
+++ EmpWageComputation.java
@@ -0,0 +1,10 @@
+package empWageComputation;
+
+public class EmpWageComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to EmpWageComputation");
+	}
+
+}
